ac0bee475cb8b32ff6d371baa4a36303fb28c065
thrazznos <nsargent.ice@gmail.com>
2023-06-15 09:52:35 -0700

    first

---
+++ ExceptionPractice/PracticeClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionPractice;
+
+public class PracticeClass
+{
+    public static void exceptionMethod()
+    {
+        throw new Exception("Something bad happened");
+        return;
+    }
+
+    public static void caughtException()
+    {
+        try
+        {
+            throw new Exception("Something bad happened, but its ok!");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"An exception was thrown: {ex.Message}");
+        }
+    }
+
+    public static void innerExceptionTest()
+    {
+        try
+        {
+            exceptionMethod();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Inner method threw an exception, but it was caught");
+        }
+    }
+}
---
+++ ExceptionPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using ExceptionPractice;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        PracticeClass.innerExceptionTest();
+    }
+}
